6a31c227e3a0b2d0725c6f8dc3598ab66cb49ffa
Djametcode <kampretsiji1@gmai.com>
2022-12-15 00:30:08 +0700

    add new feature

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
 import GroupsIcon from '@mui/icons-material/Groups';
 import Img from './assets/imported-photo-259339.thumb.png.ba96c61c9080b7723cb6f7410f8fb604.png'
 import DoneAllIcon from '@mui/icons-material/DoneAll';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import MarkChatUnreadIcon from '@mui/icons-material/MarkChatUnread';
+import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
+import VideocamIcon from '@mui/icons-material/Videocam';
+import LinkIcon from '@mui/icons-material/Link';
+import GifIcon from '@mui/icons-material/Gif';
+import HeadphonesIcon from '@mui/icons-material/Headphones';
+import SubjectIcon from '@mui/icons-material/Subject';
 
 const Main = () => {
   return (
@@ -213,12 +221,67 @@ const Header = () => {
     <div className=" flex justify-end bg-gray-800 pt-3">
       <p className=" font-sand absolute left-0 p-4 text-2xl text-slate-400">Whatsapp</p>
       <div className=" flex gap-2 p-5 text-slate-400">
-        <CameraAltOutlinedIcon/>
-        <SearchOutlinedIcon />
-        <MoreVertOutlinedIcon />
+        <CameraAltOutlinedIcon className=" cursor-pointer" onClick={alertCamera}/>
+        <SearchOutlinedIcon className=" cursor-pointer" onClick={expandSearch}/>
+        <MoreVertOutlinedIcon className=" cursor-pointer"/>
       </div>
     </div>
   )
+}
+const ExpandSearch = () => {
+  return (
+    <div className=" bg-slate-600 p-4 flex flex-col gap-3">
+      <div className=" flex gap-2">
+        <ArrowBackIcon onClick={closeExpand} className=" text-slate-400"/>
+        <input className=" border-none outline-none bg-transparent font-sand" type="text" name="" id="" placeholder="Cari..."/>
+      </div>
+      <div className=" flex gap-3">
+        <div className=" bg-gray-500 flex gap-1 font-sand text-xs p-1 rounded-2xl">
+            <MarkChatUnreadIcon fontSize="small"/>
+            <p className=" text-white">Belum dibaca</p>
+          </div>
+          <div className=" bg-gray-500 flex gap-1 font-sand text-xs p-1 pr-3 rounded-2xl">
+            <InsertPhotoIcon fontSize="small"/>
+            <p className=" text-white">Photos</p>
+          </div>
+          <div className=" bg-gray-500 flex gap-1 font-sand text-xs p-1 pr-2 rounded-2xl">
+            <VideocamIcon fontSize="small"/>
+            <p className=" text-white">Videos</p>
+          </div>
+      </div>
+      <div className=" flex gap-3">
+          <div className=" bg-gray-500 flex gap-1 font-sand text-xs p-1 pr-3 rounded-2xl">
+            <LinkIcon fontSize="small"/>
+            <p className=" text-white">Links</p>
+          </div>
+          <div className=" bg-gray-500 flex gap-1 font-sand text-xs p-1 pr-3 rounded-2xl">
+            <GifIcon fontSize="small"/>
+            <p className=" text-white">Gifs</p>
+          </div>
+          <div className=" bg-gray-500 flex gap-1 font-sand text-xs p-1 pr-3 rounded-2xl">
+            <HeadphonesIcon fontSize="small"/>
+            <p className=" text-white">Audio</p>
+          </div>
+      </div>
+      <div className=" flex flex-row gap-2">
+        <div className=" bg-gray-500 flex gap-1 font-sand text-xs p-1 pr-3 rounded-2xl">
+          <SubjectIcon fontSize="small"/>
+          <p className=" text-white">Documents</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+function expandSearch () {
+  const element = document.getElementById('ExpandSearch');
+  element.style.display = 'block';
+}
+function closeExpand () {
+  const element = document.getElementById('ExpandSearch');
+  element.style.display = 'none';
+}
+function alertCamera () {
+  window.alert('Belum bisa ngoding ini ya tod')
 }
 
 const Header2 = () => {
@@ -236,8 +299,11 @@ const Whatsapp = () => {
   return (
     <div className=" md:hidden">
       <div className=" sticky top-0 z-20">
-      <Header />
-      <Header2 />
+        <Header />
+        <Header2 />
+      <div id="ExpandSearch" className=" fixed top-0 w-full hidden">
+        <ExpandSearch/>
+      </div>
       </div>
       <Main />
     </div>
