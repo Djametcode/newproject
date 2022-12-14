f48b572b1502bd59b0790422208076770ce20d19
Djametcode <kampretsiji1@gmai.com>
2022-12-14 23:42:00 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,253 @@
+import React from "react";
+import CameraAltOutlinedIcon from '@mui/icons-material/CameraAltOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
+import GroupsIcon from '@mui/icons-material/Groups';
+import Img from './assets/imported-photo-259339.thumb.png.ba96c61c9080b7723cb6f7410f8fb604.png'
+import DoneAllIcon from '@mui/icons-material/DoneAll';
+
+const Main = () => {
+  return (
+    <>
+    <div className=" bg-slate-800 text-slate-400 flex p-3 font-sand">
+      <img className=" rounded-full w-14 h-14 mr-5" src={Img} alt="kk" srcset="" />
+      <div className=" mt-2">
+        <p>Djamet Coder</p>
+        <div className=" flex gap-1">
+          <DoneAllIcon fontSize="small" />
+          <p className=" text-xs">Hello world</p>
+        </div>
+      </div>
+      <div>
+        <p className=" z-10 absolute right-4 translate-y-2 text-xs">22:10</p>
+      </div>
+    </div>
+    <div className=" bg-slate-800 text-slate-400 flex p-3 font-sand">
+      <img className=" rounded-full w-14 h-14 mr-5" src={Img} alt="kk" srcset="" />
+      <div className=" mt-2">
+        <p>Konco Asu</p>
+        <div className=" flex gap-1">
+          <DoneAllIcon fontSize="small" />
+          <p className=" text-xs">Info Loker Su</p>
+        </div>
+      </div>
+      <div>
+        <p className=" absolute right-4 translate-y-2 text-xs">22:10</p>
+      </div>
+    </div>
+    <div className=" bg-slate-800 text-slate-400 flex p-3 font-sand">
+      <img className=" rounded-full w-14 h-14 mr-5" src={Img} alt="kk" srcset="" />
+      <div className=" mt-2">
+        <p>Wong Ilang</p>
+        <div className=" flex gap-1">
+          <DoneAllIcon fontSize="small" />
+          <p className=" text-xs">Taukah Kamu</p>
+        </div>
+      </div>
+      <div>
+        <p className=" absolute right-4 translate-y-2 text-xs">22:10</p>
+      </div>
+    </div>
+    <div className=" bg-slate-800 text-slate-400 flex p-3 font-sand">
+      <img className=" rounded-full w-14 h-14 mr-5" src={Img} alt="kk" srcset="" />
+      <div className=" mt-2">
+        <p>Slamet Kopling</p>
+        <div className=" flex gap-1">
+          <DoneAllIcon fontSize="small" />
+          <p className=" text-xs">Mabar lek bucin ae asu</p>
+        </div>
+      </div>
+      <div>
+        <p className=" absolute right-4 translate-y-2 text-xs">22:10</p>
+      </div>
+    </div>
+    <div className=" bg-slate-800 text-slate-400 flex p-3 font-sand">
+      <img className=" rounded-full w-14 h-14 mr-5" src={Img} alt="kk" srcset="" />
+      <div className=" mt-2">
+        <p>Bos Jangkrik</p>
+        <div className=" flex gap-1">
+          <DoneAllIcon fontSize="small" />
+          <p className=" text-xs">Info</p>
+        </div>
+      </div>
+      <div>
+        <p className=" absolute right-4 translate-y-2 text-xs">22:10</p>
+      </div>
+    </div>
+    <div className=" bg-slate-800 text-slate-400 flex p-3 font-sand">
+      <img className=" rounded-full w-14 h-14 mr-5" src={Img} alt="kk" srcset="" />
+      <div className=" mt-2">
+        <p>Virtual</p>
+        <div className=" flex gap-1">
+          <DoneAllIcon fontSize="small" />
+          <p className=" text-xs">Gua Wibu dan Bangga</p>
+        </div>
+      </div>
+      <div>
+        <p className=" absolute right-4 translate-y-2 text-xs">22:10</p>
+      </div>
+    </div>
+    <div className=" bg-slate-800 text-slate-400 flex p-3 font-sand">
+      <img className=" rounded-full w-14 h-14 mr-5" src={Img} alt="kk" srcset="" />
+      <div className=" mt-2">
+        <p>+6285710007432</p>
+        <div className=" flex gap-1">
+          <DoneAllIcon fontSize="small" />
+          <p className=" text-xs">Hello world</p>
+        </div>
+      </div>
+      <div>
+        <p className=" absolute right-4 translate-y-2 text-xs">22:10</p>
+      </div>
+    </div>
+    <div className=" bg-slate-800 text-slate-400 flex p-3 font-sand">
+      <img className=" rounded-full w-14 h-14 mr-5" src={Img} alt="kk" srcset="" />
+      <div className=" mt-2">
+        <p>+6285710007432</p>
+        <div className=" flex gap-1">
+          <DoneAllIcon fontSize="small" />
+          <p className=" text-xs">Hello world</p>
+        </div>
+      </div>
+      <div>
+        <p className=" absolute right-4 translate-y-2 text-xs">22:10</p>
+      </div>
+    </div>
+    <div className=" bg-slate-800 text-slate-400 flex p-3 font-sand">
+      <img className=" rounded-full w-14 h-14 mr-5" src={Img} alt="kk" srcset="" />
+      <div className=" mt-2">
+        <p>+6285710007432</p>
+        <div className=" flex gap-1">
+          <DoneAllIcon fontSize="small" />
+          <p className=" text-xs">Hello world</p>
+        </div>
+      </div>
+      <div>
+        <p className=" absolute right-4 translate-y-2 text-xs">22:10</p>
+      </div>
+    </div>
+    <div className=" bg-slate-800 text-slate-400 flex p-3 font-sand">
+      <img className=" rounded-full w-14 h-14 mr-5" src={Img} alt="kk" srcset="" />
+      <div className=" mt-2">
+        <p>+6285710007432</p>
+        <div className=" flex gap-1">
+          <DoneAllIcon fontSize="small" />
+          <p className=" text-xs">Hello world</p>
+        </div>
+      </div>
+      <div>
+        <p className=" absolute right-4 translate-y-2 text-xs">22:10</p>
+      </div>
+    </div>
+    <div className=" bg-slate-800 text-slate-400 flex p-3 font-sand">
+      <img className=" rounded-full w-14 h-14 mr-5" src={Img} alt="kk" srcset="" />
+      <div className=" mt-2">
+        <p>+6285710007432</p>
+        <div className=" flex gap-1">
+          <DoneAllIcon fontSize="small" />
+          <p className=" text-xs">Hello world</p>
+        </div>
+      </div>
+      <div>
+        <p className=" absolute right-4 translate-y-2 text-xs">22:10</p>
+      </div>
+    </div>
+    <div className=" bg-slate-800 text-slate-400 flex p-3 font-sand">
+      <img className=" rounded-full w-14 h-14 mr-5" src={Img} alt="kk" srcset="" />
+      <div className=" mt-2">
+        <p>+6285710007432</p>
+        <div className=" flex gap-1">
+          <DoneAllIcon fontSize="small" />
+          <p className=" text-xs">Hello world</p>
+        </div>
+      </div>
+      <div>
+        <p className=" absolute right-4 translate-y-2 text-xs">22:10</p>
+      </div>
+    </div>
+    <div className=" bg-slate-800 text-slate-400 flex p-3 font-sand">
+      <img className=" rounded-full w-14 h-14 mr-5" src={Img} alt="kk" srcset="" />
+      <div className=" mt-2">
+        <p>+6285710007432</p>
+        <div className=" flex gap-1">
+          <DoneAllIcon fontSize="small" />
+          <p className=" text-xs">Hello world</p>
+        </div>
+      </div>
+      <div>
+        <p className=" absolute right-4 translate-y-2 text-xs">22:10</p>
+      </div>
+    </div>
+    <div className=" bg-slate-800 text-slate-400 flex p-3 font-sand">
+      <img className=" rounded-full w-14 h-14 mr-5" src={Img} alt="kk" srcset="" />
+      <div className=" mt-2">
+        <p>+6285710007432</p>
+        <div className=" flex gap-1">
+          <DoneAllIcon fontSize="small" />
+          <p className=" text-xs">Hello world</p>
+        </div>
+      </div>
+      <div>
+        <p className=" absolute right-4 translate-y-2 text-xs">22:10</p>
+      </div>
+    </div>
+    <div className=" bg-slate-800 text-slate-400 flex p-3 font-sand">
+      <img className=" rounded-full w-14 h-14 mr-5" src={Img} alt="kk" srcset="" />
+      <div className=" mt-2">
+        <p>+6285710007432</p>
+        <div className=" flex gap-1">
+          <DoneAllIcon fontSize="small" />
+          <p className=" text-xs">Hello world</p>
+        </div>
+      </div>
+      <div>
+        <p className=" absolute right-4 translate-y-2 text-xs">22:10</p>
+      </div>
+    </div>
+    </>
+  )
+}
+
+const Header = () => {
+  return (
+    <div className=" flex justify-end bg-gray-800 pt-3">
+      <p className=" font-sand absolute left-0 p-4 text-2xl text-slate-400">Whatsapp</p>
+      <div className=" flex gap-2 p-5 text-slate-400">
+        <CameraAltOutlinedIcon/>
+        <SearchOutlinedIcon />
+        <MoreVertOutlinedIcon />
+      </div>
+    </div>
+  )
+}
+
+const Header2 = () => {
+  return (
+    <div className=" flex justify-around bg-gray-800 m-0 text-slate-400 font-sand">
+      <GroupsIcon className=" hover:bg-slate-400 translate-y-1 translate-x-2 w-full"/>
+      <p className=" hover:text-green-600 hover:bg-slate-400 w-full text-center p-1">Chat</p>
+      <p className=" hover:text-green-600 hover:bg-slate-400 w-full text-center p-1">Status</p>
+      <p className=" hover:text-green-600 hover:bg-slate-400 w-full text-center p-1">Panggilan</p>
+    </div>
+  )
+}
+
+const Whatsapp = () => {
+  return (
+    <div className=" md:hidden">
+      <div className=" sticky top-0 z-20">
+      <Header />
+      <Header2 />
+      </div>
+      <Main />
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <Whatsapp />
+  )
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/App.jsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sand': ['Quicksand', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
